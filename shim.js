3ca83bb160e53fb721c9d803ecfd3f7abb7e46dc
Jordan Harband <ljharb@gmail.com>
2017-07-14 13:41:05 -0700

    [Refactor] shim: Remove unnecessary `!functionsHaveNames` check

--- shim.js
+++ shim.js
@@ -14,24 +14,22 @@ module.exports = function shimName() {
 	if (!supportsDescriptors) {
 		throw new TypeErr('Shimming Function.prototype.name support requires ES5 property descriptor support.');
 	}
-	if (function foo() {}.name !== 'foo') {
-		var functionProto = Function.prototype;
-		defineProperty(functionProto, 'name', {
-			configurable: true,
-			enumerable: false,
-			get: function () {
-				var name = polyfill.call(this);
-				if (this !== functionProto) {
-					defineProperty(this, 'name', {
-						configurable: true,
-						enumerable: false,
-						value: name,
-						writable: false
-					});
-				}
-				return name;
+	var functionProto = Function.prototype;
+	defineProperty(functionProto, 'name', {
+		configurable: true,
+		enumerable: false,
+		get: function () {
+			var name = polyfill.call(this);
+			if (this !== functionProto) {
+				defineProperty(this, 'name', {
+					configurable: true,
+					enumerable: false,
+					value: name,
+					writable: false
+				});
 			}
-		});
-	}
+			return name;
+		}
+	});
 	return polyfill;
 };
